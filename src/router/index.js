fcda71e2669d78fa4224ad8db8d7cc202304bdd3
yft <202306110141@qq.com>
2025-10-08 16:48:25 +0800

    第四周:第三,四小节完成,进度至:index页面整体布局,左侧导航栏

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // 导入
 import Home from '@/views/Home.vue'
+import index from '@/views/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: index,
     },
     {
       path: '/login',
